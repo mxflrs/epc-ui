a8eaf6ff2708388a39b7af57facc6926d1b16a36
Max Flores <max@maxflores.dev>
2024-03-24 06:45:46 -0600

    chore: add font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        epc: {
+          r0: '#FF5E59',
+          r1: '#FF4545',
+          r2: '#DE3B3B',
+          d0: '#212121',
+          d1: '#21212150',
+          d2: '#21212125',
+          w0: '#FFFFFF',
+          w1: '#FFFFFF50',
+          w2: '#FFFFFF25'
+        },
+
+      }
+    },
   },
   plugins: [],
 }
