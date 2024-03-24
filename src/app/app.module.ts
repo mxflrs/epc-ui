3b89ce202f7bcebb777121f5bfbe9d4a55c30093
Max Flores <max@maxflores.dev>
2024-03-24 12:01:19 -0600

    chore: create main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './modules/about/about.component';
+import { ServiciosComponent } from './modules/servicios/servicios.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ServiciosComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
