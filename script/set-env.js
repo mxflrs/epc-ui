635410c189e990c213122993dd99c5b557c76b61
Max Flores <55513149+maxflorex@users.noreply.github.com>
2024-05-28 06:55:49 -0600

    chore: create script for env variables

---
+++ script/set-env.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+const envFilePath = path.join(__dirname, '../src/environments/environment.prod.ts');
+const templateFilePath = path.join(__dirname, '../src/environments/environment.prod.template.ts');
+
+fs.readFile(templateFilePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading template file:', err);
+    process.exit(1);
+  }
+
+  const result = data
+    .replace('PROJECT_ID_PLACEHOLDER', process.env.PROJECT_ID)
+    .replace('DATASET_PLACEHOLDER', process.env.DATASET)
+    .replace('API_VERSION_PLACEHOLDER', process.env.API_VERSION);
+
+  fs.writeFile(envFilePath, result, 'utf8', (err) => {
+    if (err) {
+      console.error('Error writing environment file:', err);
+      process.exit(1);
+    }
+    console.log('Environment file created successfully.');
+  });
+});
---
+++ src/environment/environment.prod.template.ts
@@ -0,0 +1,10 @@
+const environment = {
+  sanity: {
+    projectId: 'PROJECT_ID_PLACEHOLDER',
+    dataset: 'DATASET_PLACEHOLDER',
+    apiVersion: 'API_VERSION_PLACEHOLDER',
+    useCdn: true,
+  }
+}
+
+export default environment;
